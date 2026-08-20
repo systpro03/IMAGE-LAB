5bdfe58f363caef40c8439f253bc034eb9c02872
systpro04 <systemprogrammer04.com>
2026-08-20 17:30:40 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import basicSsl from "@vitejs/plugin-basic-ssl";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss(), basicSsl()],
+	plugins: [react(), tailwindcss()],
 	server: {
 		host: "0.0.0.0",
 		port: 3000,
-		https: true,
 	},
 });
